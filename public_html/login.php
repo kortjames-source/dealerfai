35cc2bc43e7fa8d18b70ceb8735a32e6b86b58a8
James Kort <kortoroy@Jamess-MacBook-Air-2.local>
2026-04-23 17:17:33 -0500

    UI: Overhaul logo with high-contrast Shield and improved sizing/transparency

--- public_html/login.php
+++ public_html/login.php
@@ -136,7 +136,7 @@ $timeoutNotice = (isset($_GET['reason']) && $_GET['reason'] === 'timeout');
   <div class="full-page-center">
     <header class="clean-header">
       <a href="index.php">
-        <img src="dealerfai_logo_modern.png" alt="DealerFAI Logo" style="height: 120px; mix-blend-mode: multiply;">
+        <img src="dealerfai_logo_modern.png" alt="DealerFAI Logo" style="height: 150px; mix-blend-mode: multiply;">
       </a>
     </header>
 
