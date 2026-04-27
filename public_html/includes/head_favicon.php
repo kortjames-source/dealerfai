17a3f21b29a5769f768529bc4b114a341a1f4fb0
James Kort <kortoroy@Jamess-MacBook-Air-2.local>
2026-04-26 19:35:07 -0500

    Update favicon to new modern logo

--- public_html/includes/head_favicon.php
+++ public_html/includes/head_favicon.php
@@ -1,7 +1,7 @@
 <?php
 // Shared favicon tags.
 ?>
-<link rel="icon" type="image/png" sizes="32x32" href="uploads/logos/FAICrest.png">
-<link rel="icon" type="image/png" sizes="16x16" href="uploads/logos/FAICrest.png">
-<link rel="shortcut icon" type="image/png" href="uploads/logos/FAICrest.png">
-<link rel="apple-touch-icon" href="uploads/logos/FAICrest.png">
+<link rel="icon" type="image/png" sizes="32x32" href="dealerfai_logo_modern_transparent.png">
+<link rel="icon" type="image/png" sizes="16x16" href="dealerfai_logo_modern_transparent.png">
+<link rel="shortcut icon" type="image/png" href="dealerfai_logo_modern_transparent.png">
+<link rel="apple-touch-icon" href="dealerfai_logo_modern_transparent.png">
